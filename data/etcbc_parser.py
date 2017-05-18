d60e3681640d85ca1c5cca146d64279f8729a3d7
Kevin <kevwolf@umich.edu>
2017-05-18 21:34:22 +0300

    Parser for whole tank

--- data/etcbc_parser.py
+++ data/etcbc_parser.py
@@ -19,58 +19,21 @@ def stripNikkud(rawString):
     return regex.sub(r"[\u0591-\u05C7]", "", rawString)
 
 
-gematria = {}
-gematria[u'א'] = 1
-gematria[u'ב'] = 2
-gematria[u'ג'] = 3
-gematria[u'ד'] = 4
-gematria[u'ה'] = 5
-gematria[u'ו'] = 6
-gematria[u'ז'] = 7
-gematria[u'ח'] = 8
-gematria[u'ט'] = 9
-gematria[u'י'] = 10
-gematria[u'כ'] = 20
-gematria[u'ל'] = 30
-gematria[u'מ'] = 40
-gematria[u'נ'] = 50
-gematria[u'ס'] = 60
-gematria[u'ע'] = 70
-gematria[u'פ'] = 80
-gematria[u'צ'] = 90
-gematria[u'ק'] = 100
-gematria[u'ר'] = 200
-gematria[u'ש'] = 300
-gematria[u'ת'] = 400
+def getShoresh(line, index):
+    index_underscore = line.rfind(u'-', 0, index)
+    index_slash = line.rfind(u'/', 0, index)
 
+    b_index = index_slash if index_slash > index_underscore else index_underscore
+    return stripNikkud(line[b_index+2:index])
 
-def getGematria(txt):
-        if not isinstance(txt, unicode):
-            txt = txt.decode('utf-8')
-        index=0
-        sum=0
-        while index <= len(txt)-1:
-            if txt[index:index+1] in gematria:
-                sum += gematria[txt[index:index+1]]
-
-            index+=1
-        return sum
+def getWholeWord(line, index):
+    return 0
 
 
 
-test_string2 = u"וַתֹּוצֵ֨א --- ‎וְ__conj__None__None__None__---__n687 /// יצא[__verb__hif__wayq__None__-3fsg__n694" \
-              u"הָאָ֜רֶץ --- ‎הַ__art__expl__None__None__---__n697 /// אֶרֶץ/__subs__None__None__a__-?sg__n697" \
-              u"דֶּ֠שֶׁא --- ‎דֶּשֶׁא/__subs__None__None__a__-msg__n701" \
-              u"עֵ֣שֶׂב --- ‎עֵשֶׂב/__subs__None__None__a__-msg__n701" \
-              u"מַזְרִ֤יעַ --- ‎זרע[__verb__hif__ptca__a__?msg__n706"
 
 # {
-#     "Genesis": {
-#         "1": {
-#             "1": [list of words]
-#         }
-#     }
-#
+#     "Genesis": [[[list of words]]]
 # }
 
 with codecs.open("mikra-morph.txt.withnikudnew.txt", 'rb', encoding='utf8') as file_read:
@@ -114,31 +77,25 @@ with codecs.open("mikra-morph.txt.withnikudnew.txt", 'rb', encoding='utf8') as f
                 book_title = new_book_title
 
         else:
-            index = line.find(u"=")
 
-            if index is not -1:
-                b_index = line.rfind(u' ', 0, index)
-                word = line[b_index+1:index]
-                pasuk.append(stripNikkud(word))
+            index = line.find(u"---")
 
-            else:
-                index = line.find(u"/_")
+            pasuk.append(stripNikkud(line[:index-1]))
 
-                if index is not -1:
-                    b_index = line.rfind(u' ', 0, index)
-                    word = line[b_index+1:index]
-                    pasuk.append(stripNikkud(word))
-
-                else:
-                    index = line.find(u"[")
-                    if index is not -1:
-                        b_index = line.rfind(u' ', 0, index)
-
-                        word = line[b_index+1:index]
-                        pasuk.append(stripNikkud(word))
+            # if index is not -1:
+            #     pasuk.append(getShoresh(line, index))
+            # else:
+            #     index = line.find(u"/_")
+            #     if index is not -1:
+            #         pasuk.append(getShoresh(line, index))
+            #     else:
+            #         index = line.find(u"[")
+            #         if index is not -1:
+            #             pasuk.append(getShoresh(line, index))
 
 
-    with codecs.open("only_shorashim.json", 'wb', encoding='utf8') as file_write:
+
+    with codecs.open("whole_tanakh.json", 'wb', encoding='utf8') as file_write:
         json.dump(books, file_write, ensure_ascii=False, indent=4)
 
     #     exact_words = wo_preps.write(stripNikkud(line))
