ca4b63e2e8f0a7265a18496af0490078bfbe7c2e
Noah Santacruz <noahssantacruz@gmail.com>
2017-05-19 04:06:23 +0300

    working with cosine dist

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from local_settings import *
 from sefaria.model import *
 import tfidf, json, codecs, glob
 
+class Keta:
+
+    def __init__(self, ref, name, words):
+        self.ref = ref
+        self.name = name
+        self.words = words
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __ne__(self, other):
+        return not(self == other)
+
+    def __str__(self):
+        return u"{} - {}".format(self.name, self.ref.normal())
+
+
 def reduce_list(list_in):
     return reduce(lambda a, b: a+b, list_in)
 
@@ -14,26 +34,40 @@ def input_ketaim_into_table(ketaim, table, level_num):
 
         with codecs.open(file_name, 'rb', encoding='utf8') as fp2:
             json_obj = json.load(fp2, encoding='utf8')
-            for keta_obj in json_obj:
-                ref = Ref(keta_obj['ref'])
-
+            for keta_dict in json_obj:
+                ref = Ref(keta_dict['ref'])
                 word_list = []
                 for sub_ref in ref.range_list():
                     word_list += ketaim[sub_ref.index.title][sub_ref.sections[0]-1][sub_ref.sections[1]-1]
-                table.addDocument(keta_obj['name'], word_list)
+                keta_obj = Keta(ref, keta_dict['name'], word_list)
+                table.addDocument(keta_obj, word_list)
+    table.finalize()
 
 def find_parallels():
-    table = tfidf.tfidf()
+
     sim_matrix = []
 
     count = 0
     with codecs.open("data/only_shorashim.json", 'rb', encoding='utf8') as fp:
         shorash_obj = json.load(fp)
+        all_words = set()
+        for k, v in shorash_obj.items():
+            if k == u"Obadiah":
+                continue
+            all_words |= set(reduce_list(reduce_list(v)))
+
+        table = tfidf.tfidf(all_words)
         input_ketaim_into_table(shorash_obj, table, 2)
-        for keta_name, keta_dict in table.documents:
-            keta_words = keta_dict.keys()
-            similarities = table.similarities(keta_words)
+        for keta_obj, keta_dict in table.documents.items():
+            similarities = table.similarities(keta_obj, 5)
             sim_matrix += similarities
+
+            for similarity in similarities:
+                if similarity[1] > 0.5:
+                    print u"{}\n\t{}\n\t{}".format(similarity[1], keta_obj, similarity[0])
+
+        pass
+
 
 
 
--- tfidf.py
+++ tfidf.py
@@ -9,51 +9,78 @@ See the README for a usage example.
 """
 
 import sys
-import os
+import os, heapq, math
+import numpy as np
 from collections import OrderedDict
 
 class tfidf:
-  def __init__(self):
+  def __init__(self, all_words):
     self.weighted = False
-    self.documents = []
+    self.documents = OrderedDict()
     self.corpus_dict = OrderedDict()
+    self.all_words = list(all_words)
+    self.word2int = {}
+    for i, w in enumerate(all_words):
+      self.word2int[w] = i
 
-  def addDocument(self, doc_name, list_of_words):
+
+  @staticmethod
+  def argmax(iterable, n=1):
+    if n == 1:
+      return max(enumerate(iterable), key=lambda x: x[1])[0]
+    else:
+      return heapq.nlargest(n, xrange(len(iterable)), iterable.__getitem__)
+
+  def addDocument(self, doc_obj, list_of_words):
     # building a dictionary
-    doc_dict = OrderedDict()
+    doc_list = np.zeros((len(self.all_words,)))
     for w in list_of_words:
-      doc_dict[w] = doc_dict.get(w, 0.) + 1.0
+      word_ind = self.word2int[w]
+      doc_list[word_ind] += 1
       self.corpus_dict[w] = self.corpus_dict.get(w, 0.0) + 1.0
 
     # normalizing the dictionary
-    length = float(len(list_of_words))
-    for k in doc_dict:
-      doc_dict[k] = doc_dict[k] / length
+    doc_list /= float(len(list_of_words))
 
     # add the normalized document to the corpus
-    self.documents.append([doc_name, doc_dict])
+    self.documents[doc_obj] = doc_list
 
-  def similarities(self, list_of_words):
-    """Returns a list of all the [docname, similarity_score] pairs relative to a list of words."""
+  def finalize(self):
+    num_docs = float(len(self.documents))
+    for w, wc in self.corpus_dict.items():
+      temp_idf = 1 + math.log(num_docs / self.corpus_dict[w])
+      for k, doc_list in self.documents.items():
+        doc_list[self.word2int[w]] *= temp_idf
 
-    # building the query dictionary
-    query_dict = OrderedDict()
-    for w in list_of_words:
-      query_dict[w] = query_dict.get(w, 0.0) + 1.0
+  def similarities(self, doc_obj, top_n_words):
+    doc_list = self.documents[doc_obj]
+    sims = []
+    for temp_doc_obj, temp_doc_list in self.documents.items():
+      if temp_doc_obj == doc_obj:
+        continue
+      cos_dist = np.dot(doc_list, temp_doc_list) / (np.linalg.norm(doc_list) * np.linalg.norm(temp_doc_list))
+      sims += [(temp_doc_obj, cos_dist)]
 
-    # normalizing the query
-    length = float(len(list_of_words))
-    for k in query_dict:
-      query_dict[k] = query_dict[k] / length
+    return sorted(sims, key=lambda x: x[1])
+
+
+
 
     # computing the list of similarities
     sims = []
-    for doc in self.documents:
-      score = 0.0
-      doc_dict = doc[1]
-      for k in query_dict:
-        if k in doc_dict:
-          score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
-      sims.append([doc[0], score])
+    # for doc in self.documents:
+    #   score_list = []
+    #   doc_dict = doc[1]
+    #   for k in query_dict:
+    #     if k in doc_dict:
+    #       score_list += [(query_dict[k] * math.log(float(len(self.corpus_dict))/self.corpus_dict[k])) + (doc_dict[k] / self.corpus_dict[k])]
+    #
+    #   top_words = []
+    #   top_word_inds = self.argmax(score_list, top_n_words)
+    #   words = query_dict.keys()
+    #   for word_ind in top_word_inds:
+    #     top_words += [words[word_ind]]
+    #
+    #   sims.append([doc[0], sum(score_list), top_words])
 
-    return sims
+    # return sims
